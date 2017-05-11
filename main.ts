5de67792a30d7f36bf04798938efb6c250593aea
Victor Garcia <vgarciabnz@gmail.com>
2017-05-11 16:42:21 +0200

    Change to webpack (broken code)

---
+++ main.ts
@@ -0,0 +1,6 @@
+import './polyfills.ts';
+
+import { platformBrowserDynamic } from './node_modules/@angular/platform-browser-dynamic';
+import { AppModule } from '.';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
